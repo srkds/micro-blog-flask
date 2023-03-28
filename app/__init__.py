e80bca796e8c34c84d95c4cfb9c8c47a59ad76a7
srkds <im.nirav86@gmail.com>
2023-03-28 22:42:09 +0530

    init microblog 🚀

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+"""
+1. Create a main project directory
+2. For every project use venv which is virtual environment and install separate specific
+versions for specific projects.
+3. activate the virtual environment and install flask
+4. Create a package(folder) named "app" 
+5. Inside app/ create __init__.py file
+6. Application exists in a package
+7. In python a subdirectory which contains __init__.py file is considered as package and can be imported
+8. When we import a package __init__.py executes and defines what symbols exposes to world 
+Now in __init__.py file import flask
+"""
+
+from flask import Flask # imoprt flask
+
+# Configure flask
+app = Flask(__name__) # here app is a variable defined as insance of class Flask
